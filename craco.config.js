650f71f6bca1ce061af5b04310df7824f6d68498
ChangHyunKim <hot9998@naver.com>
2021-02-25 23:15:57 +0900

    Setting up typescript alias path

---
+++ craco.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@src': path.resolve(__dirname, 'src/'),
+    },
+  },
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        '^@(.*)$': '<rootDir>/src$1',
+      },
+    },
+  },
+};
